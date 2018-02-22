9fc371c6fe837c0c62ee3bfc36c5168c48cc5686
Katherine MacKenzie <katherinemackenzie@Katherines-MacBook-Air.local>
2018-02-22 20:17:17 +0000

    Adding JS folder

---
+++ index.js
@@ -0,0 +1,11 @@
+<script>
+  function CheckC() {
+    var x = document.forms["AnswerC"]["MyAnswerC"].value;
+    if (x == IWILLWAITFORYOUATTHEBRIDGE) {
+      alert("Well done! You cracked the code!");
+    }
+    else {
+      alert("Oops! Try again");
+    }
+  }
+</script>
