e12c71a2ac266549411a895e77ea406beb48ac46
Amy Kidd <amykidd1999@gmail.com>
2018-02-28 18:36:23 +0000

    Answer Checker Working

--- Email.php
+++
@@ -1,2 +0,0 @@
-
-Thank you for you feedback, <?php echo $_POST["name"]; ?>
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 <script>
-  function CheckC() {
-    var x = document.forms["AnswerC"]["MyAnswerC"].value;
-    if (x == IWILLWAITFORYOUATTHEBRIDGE) {
-      alert("Well done! You cracked the code!");
-    }
-    else {
-      alert("Oops! Try again");
-    }
-  }
+
+function CheckC() {
+var x = document.forms['AnsC']['CaesAns'].value;
+
+if (x == 'I will wait for you at the bridge') {
+  alert('Well done! You cracked the code!');
+}
+else {
+  alert('Oops! Try again');
+}
+}
+
 </script>
