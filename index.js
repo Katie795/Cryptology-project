c2e0ba1f6b01736c80629af9291a7031dbce1090
Katherine MacKenzie <katherinemackenzie@Katherines-MacBook-Air.local>
2018-03-04 19:35:25 +0000

    Rewording the content

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ else {
 }
 }
 
+function CheckC() {
+var x = document.forms['AnsC']['MonoAns'].value;
+
+if (x == "Problems worthy of attack prove their worth by fighting back - Paul Erdos") {
+  alert('Well done! You cracked the code!');
+}
+else {
+  alert('Oops! Have another go.');
+}
+}
+
+function Hint() {
+  alert('Hint')
+}
+
 
 
 $(document).ready(function(){
